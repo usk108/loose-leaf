36ca99d3b0f077db65f785b7c63b01cfa5db7ff5
usk108 <clair.couleur.ciel.108@gmail.com>
2015-11-17 09:46:08 +0900

    create index on ES

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -14,6 +14,10 @@ class Memo < ActiveRecord::Base
   validates :text, presence: true
   belongs_to :user
 
+  include Elasticsearch::Model
+
+  index_name "memo_#{Rails.env}" #インデックス名を指定
+
   def show_date
     "#{self.date.year}年#{self.date.month}月#{self.date.day}日"
   end
